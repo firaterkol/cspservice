ca9f5d535162cb07b784f500a6940ca35712d5b9
Firat Erkol <firat.erkol@bertelsmann.de>
2023-12-07 15:22:29 +0100

    model change

--- backend/Models/Report.model.js
+++ backend/Models/Report.model.js
@@ -1,16 +1,96 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const CspSchema = new Schema({
+    "document-uri":{
+        type: String,
+        required: false
+    },
+
+    referrer: {
+        type: String,
+        required: false
+    },
+
+    "blocked-uri": {
+        type: String,
+        required: false
+    },
+
+    "violated-directive": {
+        type: String,
+        required: false
+    },
+
+    "original-policy": {
+        type: String,
+        required: false
+    },
+
+    "source-file": {
+        type: String,
+        required: false
+    },
+
+    "line-number": {
+        type: Number,
+        required: false
+    },
+
+    "column-number": {
+        type: Number,
+        required: false
+    },
+
+    "effective-directive": {
+        type: String,
+        required: false
+    },
+    
+    "status-code": {
+        type: Number,
+        required: false
+    },
+
+    "status-message": {
+        type: String,
+        required: false
+    },
+
+    "script-sample": {
+        type: String,
+        required: false
+    },
+
+    disposition: {
+        type: String,
+        required: false
+    },
+
+    "referrer-policy": {
+        type: String,
+        required: false
+    }
+});
+
 const ReportSchema = new Schema({
-    message: {
+    domain: {
         type: String,
         required: true
     },
-    url: {
-        type: String,
+
+    date: {
+        type: Date,
+        required: true
+    },
+    
+    'csp-report': {
+        type: CspSchema,
         required: true
     }
 });
+
+
 
 const Report = mongoose.model('report', ReportSchema);
 module.exports = Report;
--- backend/initDB.js
+++ backend/initDB.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 module.exports = () => {
     //  mongodb://127.0.0.1:27017/cspReports
-    mongoose.connect('mongodb://mongodb:27017/cspReports').then(() => {
+    //  mongodb://mongodb:27017/cspReports
+    mongoose.connect('mongodb://127.0.0.1:27017/cspReports').then(() => {
         console.log('Mongodb connected');
     })
         .catch(err => console.log(error.message));
